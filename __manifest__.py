b30bbe9fc3bb3ddb6325bf8deb2f21d67e9eb0de
Luca-Cocozza <luca.cocozza@futurasl.com>
2024-02-12 17:24:08 +0100

    Second commit

--- __manifest__.py
+++ __manifest__.py
@@ -5,13 +5,13 @@
     'application': True,
     'description': "This module is used to manage the photos taken of the vehicles by the facility.",
     'depends': [
-        '',],
+        'survey',],
     'data': [
         # # # Settaggi per accesso ai contenuti
-        # 'data/ir.model.access.csv',
+        'data/ir.model.access.csv',
         # # # Caricamento delle view,
-        # 'view/res_partner_view_update.xml',
+        'view/fleet_check.xml',
         # # Menu
-        # 'view/menu.xml',
+        'view/menu.xml',
     ],
 }
